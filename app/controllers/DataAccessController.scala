88519b0098eff76a1ee0e6592da819bd38c1e05d
mmr.K <pollvaulter.m@gmail.com>
2017-08-31 09:32:24 +0900

    add comment

--- app/controllers/DataAccessController.scala
+++ app/controllers/DataAccessController.scala
@@ -6,6 +6,11 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import play.api.mvc._
 import dao.testDao
 
+/**
+  * created by mmr 2017/08/31
+  * DataAccessTest Controller
+  */
+
 @Singleton
 class DataAccessController @Inject()(testDao: testDao) extends Controller {
 
