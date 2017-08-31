a95022d04c4eab9e4cf61015b1a8c0bf6912e0fa
mmr.K <pollvaulter.m@gmail.com>
2017-08-31 11:19:12 +0900

    add form object

--- app/controllers/DataAccessController.scala
+++ app/controllers/DataAccessController.scala
@@ -1,10 +1,13 @@
 package controllers
 
 import javax.inject.{Inject, Singleton}
-import scala.concurrent.ExecutionContext.Implicits.global
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import play.api.mvc._
+import play.api.data.Forms._
 import dao.testDao
+import models.Test
+import play.api.data.Form
 
 /**
   * created by mmr 2017/08/31
@@ -14,9 +17,19 @@ import dao.testDao
 @Singleton
 class DataAccessController @Inject()(testDao: testDao) extends Controller {
 
+  //データの入力用フォーム作成
+  val inputform = Form(mapping(
+    "id"->longNumber,
+    "name"->text)
+  (Test.apply)(Test.unapply)
+  )
+
+
+
+  //  データの表示
   def show = Action.async {
     testDao.all().map {
-      test => Ok(views.html.result("出力")(test))
+      test => Ok(views.html.result("DBテスト")(test))
     }
   }
 }
