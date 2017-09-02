efccd9068f2e1f140db0b8a32a8d3276d97da021
mmr.K <pollvaulter.m@gmail.com>
2017-09-02 13:42:25 +0900

    add edit method dao

--- app/dao/testDao.scala
+++ app/dao/testDao.scala
@@ -10,15 +10,25 @@ import slick.driver.JdbcProfile
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-class testDao @Inject()(protected val dbConfigProvider: DatabaseConfigProvider) extends HasDatabaseConfigProvider[JdbcProfile]{
+class testDao @Inject()(protected val dbConfigProvider: DatabaseConfigProvider) extends HasDatabaseConfigProvider[JdbcProfile] {
+
   import driver.api._
+
   private val Tests = TableQuery[TestTable]
 
-  private class TestTable(tag: Tag) extends Table[Test](tag,"connection"){
-    def id = column[Long]("id",O.PrimaryKey,O.AutoInc)
+  private class TestTable(tag: Tag) extends Table[Test](tag, "connection") {
+    def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+
     def name = column[String]("name")
-    def * = (id,name)<>(Test.tupled,Test.unapply _)
+
+    def * = (id, name) <> (Test.tupled, Test.unapply _)
   }
-  def all():Future[Seq[Test]] = db.run(Tests.result)
-  def insert(test:Test):Future[Unit]=db.run(Tests += test).map{ _ => ()}
+
+  def all(): Future[Seq[Test]] = db.run(Tests.result)
+
+  def insert(test: Test): Future[Unit] = db.run(Tests += test).map { _ => () }
+
+  def edit(test: Test): Future[Int] = db.run(Tests.filter(
+    _.id === test.id
+  ).map(_.name).update(test.name))
 }
