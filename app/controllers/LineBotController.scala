582d763a57eea11f769a2ca381dabef6ccaa1615
mmr.K <pollvaulter.m@gmail.com>
2017-09-06 12:49:23 +0900

    add mapping for case class to json

--- app/controllers/LineBotController.scala
+++ app/controllers/LineBotController.scala
@@ -15,10 +15,18 @@ import scala.util.parsing.json.JSONArray
 @Singleton
 class LineBotController @Inject()(testDao: testDao)(val messagesApi: MessagesApi) extends Controller with I18nSupport{
 
+  implicit val testWrites = new Writes[Test] {
+    override def writes(o: Test): JsValue = Json.obj(
+      "id" -> o.id,
+      "name" -> o.name
+    )
+  }
+
   def getJsonString = Action.async {
     testDao.all().map(
       datalist=>{
-       var jsonlist = datalist.toList.toString()
+//       var jsonlist = JSONArray(datalist.toList).toString()
+       var jsonlist = Json.toJson(datalist).toString()
         Ok(jsonlist)
       }
     )
