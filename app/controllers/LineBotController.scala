0e124bf555ac7b7b9ab275dbc66da5d99aa4ace2
mmr.K <pollvaulter.m@gmail.com>
2017-09-07 11:59:56 +0900

    create post method for json

--- app/controllers/LineBotController.scala
+++ app/controllers/LineBotController.scala
@@ -5,7 +5,10 @@ import javax.inject.{Inject, Singleton}
 import scala.concurrent.ExecutionContext.Implicits.global
 import play.api.libs.json._
 import dao.testDao
-import models.Test
+import models.{Test, TestJsonData}
+import play.api.Logger
+import play.api.data.Form
+import play.api.data.Forms.{mapping, nonEmptyText}
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.Controller
 import play.api.mvc._
@@ -13,8 +16,9 @@ import play.api.mvc._
 import scala.util.parsing.json.JSONArray
 
 @Singleton
-class LineBotController @Inject()(testDao: testDao)(val messagesApi: MessagesApi) extends Controller with I18nSupport{
+class LineBotController @Inject()(testDao: testDao)(val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
+  //mapping class for json
   implicit val testWrites = new Writes[Test] {
     override def writes(o: Test): JsValue = Json.obj(
       "id" -> o.id,
@@ -22,18 +26,32 @@ class LineBotController @Inject()(testDao: testDao)(val messagesApi: MessagesApi
     )
   }
 
+  implicit val JsonRead = Json.reads[TestJsonData]
+
   def getJsonString = Action.async {
     testDao.all().map(
-      datalist=>{
-//       var jsonlist = JSONArray(datalist.toList).toString()
-       var jsonlist = Json.toJson(datalist).toString()
+      datalist => {
+        //       var jsonlist = JSONArray(datalist.toList).toString()
+        var jsonlist = Json.toJson(datalist).toString()
         Ok(jsonlist)
       }
     )
   }
 
-  def getTestJson = Action{
-    var jsondata = "Test"
-    Ok(jsondata)
+
+  def sendPHP = Action.async { request =>
+    //bodyを代入
+    val formdata = request.body.asJson
+    var json_request = Json.toJson(formdata).validate[TestJsonData]
+    var jsonlist: String = ""
+    var coun:Int = 0
+    testDao.all().map(
+      datalist => {
+        jsonlist = Json.toJson(datalist).toString()
+        Ok(jsonlist)
+      }
+    )
+
   }
+
 }
